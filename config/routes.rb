00120f15981e148e4595c6ba15f8ab205ec8a709
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-05-05 18:24:18 +0000

    add admin/categories routes and templates, revise about route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
-  get 'about/index'
+  namespace :admin do
+    get 'categories/index'
+    get 'categories/new'
+    get 'categories/create'
+  end
+  # get 'about/index'
   get 'static_pages/help'
   get 'static_pages/faq'
   get 'static_pages/about'
   root to: 'products#index'
 
+  resources :about
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -19,6 +25,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
