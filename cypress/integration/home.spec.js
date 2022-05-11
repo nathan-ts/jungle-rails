c09dd44864a6003ff823132a772227c1e966a254
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-05-11 01:39:02 +0000

    add testing to home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,22 +2,19 @@
 
 describe('Jungle app', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
   })
 
-  it('displays two todo items by default', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
+  it('displays the intro section by default', () => {
     cy.get('.intro').should('be.visible')
-
-    // We can go even further and check that the default todos each contain
-    // the correct text. We use the `first` and `last` functions
-    // to get just the first and last matched elements individually,
-    // and then perform an assertion with `should`.
-
-    // cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    // cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
   })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 
 })
